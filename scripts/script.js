6330fe2828ae25d7386295cd5668feaabdfaae50
Nathan Quan <nathansquan@gmail.com>
2023-04-05 21:29:20 -0400

    Create boilerplate for each object needed
    - gameBoard, displayController using the module pattern
    - playerFactory using a factory function

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// gameboard object using module pattern
+const gameBoard = (() => {
+
+})();
+
+// object to control flow of the game using module pattern
+const displayController = (() => {
+
+})();
+
+// player object factory function
+const playerFactory = name => {
+
+}
