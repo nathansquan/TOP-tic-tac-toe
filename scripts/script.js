3def9ce0a4a01560d53fcdd5efdfb7d17ef2f5c0
Nathan Quan <nathansquan@gmail.com>
2023-04-06 21:53:12 -0400

    create function to render gameboard contents
    - set up and style 3x3 grid for tic tac toe using html and Bootstrap
    - create gameBoard.renderBoard() method

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
 // gameboard object using module pattern
 const gameBoard = (() => {
+    let _board = [
+        ["X", "X", "X"],
+        ["X", "X", "X"],
+        ["X", "X", "X"],
+    ];
 
+    const _cells = document.querySelectorAll(".cell");
+
+    const renderBoard = () => {
+        const flatBoard = _board.flat();
+        
+        [..._cells].map((cell, boardCell) => {
+            cell.innerText = flatBoard[boardCell];
+        });
+    };
+
+    return {renderBoard};
 })();
 
 // object to control flow of the game using module pattern
